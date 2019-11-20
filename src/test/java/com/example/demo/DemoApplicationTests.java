d85f752bb7a6ef395965ab92487aadbb4397472c
wanzhs_cedar <wanzhs_cedar@163.com>
2019-11-20 11:12:52 +0800

    空项目调试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,6 +8,17 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    void test1(){
+        System.out.println("=============test1=================");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("=============test2=================");
     }
 
 }
